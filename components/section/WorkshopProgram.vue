90867ec14f9078754a9829f63ef6255842b54e64
mahmoud <mahmoud.kheyrati.fard@gmail.com>
2024-08-15 17:32:38 +0330

    fix: text alignment

--- components/section/WorkshopProgram.vue
+++ components/section/WorkshopProgram.vue
@@ -41,7 +41,7 @@ import { workshop_program } from "~/data.json";
         </template>
 
         <template #item="{ item }">
-          <p class="lg:ps-[200px] mt-2">
+          <p class="lg:ps-[200px] mt-2 text-justify">
             <span v-if="item.speaker!=''" class="font-bold">Abstract: </span>
             <span v-else class="">Have a tea or coffee and then come back</span>
 
