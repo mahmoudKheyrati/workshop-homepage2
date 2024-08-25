487f83d59f7747876b80341d117b309089eb5479
mahmoud <mahmoud.kheyrati.fard@gmail.com>
2024-08-25 11:58:14 +0330

    fix: finalized website news

--- components/app/News.vue
+++ components/app/News.vue
@@ -1,4 +1,5 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+</script>
 <template>
   <div
     class="max-h-full bg-white/85 rounded-xl flex flex-col gap- text-gray-1000 dark:text-black/70 p-4 "
@@ -14,6 +15,24 @@
     />
 
     <div class="overflow-y-auto flex-1 flex flex-col">
+      <p class="grid grid-cols-6 gap-2 lg:gap-0 pt-3">
+        <span class="col-span-1 opacity-75 text-sm text-center text-blue-950"
+        >25 Aug. 2024</span
+        >
+        <span class="col-span-5">
+          Workshop program finalized.
+        </span>
+      </p>
+      <UDivider
+        size="xs"
+        type="dotted"
+        class="py-2"
+        :ui="{
+          border: {
+            base: 'flex border-gray-600 dark:border-gray-800',
+          },
+        }"
+      />
       <p class="grid grid-cols-6 gap-2 lg:gap-0">
         <span class="col-span-1 opacity-75 text-sm text-center text-blue-950"
           >4 Aug. 2024</span
@@ -33,25 +52,6 @@
           },
         }"
       />
-   <div class="overflow-y-auto flex-1 flex flex-col">
-        <p class="grid grid-cols-6 gap-2 lg:gap-0">
-          <span class="col-span-1 opacity-75 text-sm text-center text-blue-950"
-            >25 Aug. 2024</span
-          >
-          <span class="col-span-5">
-            Workshop programs is finalized.
-          </span>
-        </p>
-        <UDivider
-          size="xs"
-          type="dotted"
-          class="py-2"
-          :ui="{
-            border: {
-              base: 'flex border-gray-600 dark:border-gray-800',
-            },
-          }"
-        />
       <p class="grid grid-cols-6">
         <span class="col-span-1 opacity-75 text-sm text-center text-blue-950"
           >4 Aug. 2024</span
