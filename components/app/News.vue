0ff7b6dfe77a3c471f6707707cbcfea056922f30
mahmoud <mahmoud.kheyrati.fard@gmail.com>
2024-09-09 17:47:11 +0330

    fix: add registration link

--- components/app/News.vue
+++ components/app/News.vue
@@ -17,6 +17,25 @@
     <div class="overflow-y-auto flex-1 flex flex-col">
       <p class="grid grid-cols-6 gap-2 lg:gap-0 pt-3">
         <span class="col-span-1 opacity-75 text-sm text-center text-blue-950"
+        >9 Sep. 2024</span
+        >
+        <span class="col-span-5">
+          Registration is open.
+        </span>
+      </p>
+      <UDivider
+        size="xs"
+        type="dotted"
+        class="py-2"
+        :ui="{
+          border: {
+            base: 'flex border-gray-600 dark:border-gray-800',
+          },
+        }"
+      />
+
+      <p class="grid grid-cols-6 gap-2 lg:gap-0 pt-3">
+        <span class="col-span-1 opacity-75 text-sm text-center text-blue-950"
         >25 Aug. 2024</span
         >
         <span class="col-span-5">
