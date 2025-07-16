b6fa86443fd4d0cd588a3abf115ff4860562754f
mahmoud <mahmoud.kheyrati.fard@gmail.com>
2025-07-16 10:45:26 +0330

    fix - update speakers

--- components/app/News.vue
+++ components/app/News.vue
@@ -20,6 +20,24 @@
         >21 Mar. 2025</span
         >
         <span class="col-span-5">
+          The workshop program is now available.
+        </span>
+      </p>
+      <UDivider
+        size="xs"
+        type="dotted"
+        class="py-2"
+        :ui="{
+          border: {
+            base: 'flex border-gray-600 dark:border-gray-800',
+          },
+        }"
+      />
+      <p class="grid grid-cols-6 gap-2 lg:gap-0 pt-3">
+        <span class="col-span-1 opacity-75 text-sm text-center text-blue-950"
+        >21 Mar. 2025</span
+        >
+        <span class="col-span-5">
           The homepage of the International Online Workshop "In-Memory and In-storage Processing" is launched.
         </span>
       </p>
