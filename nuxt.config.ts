5c1de8047091bcb7cfc059bb2337f9e1de06cb36
mahmoud <mahmoud.kheyrati.fard@gmail.com>
2025-08-30 10:13:50 +0330

    fix(internal): version 22

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+    baseURL: '/workshop-in-memory-workshop-webpage/',  // <- set this to your GitHub Pages repo path
+  },
 
   compatibilityDate: "2024-04-03",
   devtools: { enabled: false },
