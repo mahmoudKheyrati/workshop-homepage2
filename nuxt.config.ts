d9120a265b6c3ae1beeda09d27ade3286286f475
mahmoud <mahmoud.kheyrati.fard@gmail.com>
2025-08-30 10:21:26 +0330

    fix(internal): version 22

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    app: {
-    baseURL: '/workshop-in-memory-workshop-webpage/',  // <- set this to your GitHub Pages repo path
-  },
-
   compatibilityDate: "2024-04-03",
   devtools: { enabled: false },
   css: ["~/assets/css/tailwind.css", "~/assets/css/global.css"],
@@ -14,4 +10,5 @@ export default defineNuxtConfig({
     },
   },
   modules: ["@nuxt/ui", "@nuxtjs/tailwindcss", "@nuxt/image", "@nuxt/icon"],
+  ssr: true,
 });
